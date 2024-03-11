7e52349936180c655473f92a6337cc3353b8dcc8
raellusionz <146325019+raellusionz@users.noreply.github.com>
2024-03-12 00:54:49 +0800

    New Update to Transaction Month

--- python/transactionlogmonth.py
+++ python/transactionlogmonth.py
@@ -20,9 +20,9 @@ for transaction in transactionmonthdata:
     }
     transactions_dict_month.append(transaction_dict)
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='../client/view')
 
-@app.route("/python/transactionlogmonth.py")
+@app.route("/transactionmonthtesting")
 
 def transaction_test(): 
     if len(transactionmonthdata) != 0:
@@ -42,13 +42,23 @@ def transaction_test():
         }
     ), 404
 
-@app.route("/python/transactionlogmonth.py")
+@app.route("/transactionlogmonth")
 def results():
+    monthspendpos = 0
+    monthspendneg = 0
+    for txn in transactions_dict_month:
+        if txn["crban"] == accountnumber:
+            monthspendpos += txn["txn_amt"]
+        if txn["drban"] == accountnumber:
+            monthspendneg += txn["txn_amt"]
+
     context = {
         "title": "Transaction By Month",
         "transactionsByMonths": transactions_dict_month,
+        "monthspendpos" : monthspendpos,
+        "monthspendneg" : monthspendneg
     }
-    return render_template("../../client/view/homepage.html", **context)
+    return render_template("transactionlogmonth.html", **context)
 
 
 if __name__ == '__main__':
