f9614815e6ab3d310425bccc1f72ff19700e6092
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-03-17 00:55:14 +0800

    Merge branch 'main' of https://github.com/raellusionz/ESDBank

---
+++ microservices/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import requests
+import json
+from invokes import invoke_http
+
+app = Flask(__name__, template_folder="../client/templates", static_folder="../client/static")
+
+@app.route("/")
+def home():
+    return render_template("homepage.html")
+
+@app.route("/getTransactionHist", methods=['POST'])    
+def getTransactionHist():
+    bankID = request.json['bankID']
+    transactionHist = invoke_http("http://127.0.0.1:5003/transactionHistory/bank_acct_id/" + str(bankID), method='GET')
+    return transactionHist
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
