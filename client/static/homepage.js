e517f331959897dc394bf90056672a8799efc64e
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2024-03-17 23:29:33 +0800

    update ui w connections to backend

--- client/static/homepage.js
+++ client/static/homepage.js
@@ -1,5 +1,11 @@
 // for testing purposes
 bankID = 123456789012
+currMonth = '02'
+
+quickTransferForm = document.getElementById("quickTransfer")
+quickTransferForm.addEventListener('submit', (event) => {
+  console.log(quickTransferForm);
+});
 
 fetch('/getTransactionHist', {
   method: 'POST',
@@ -10,7 +16,8 @@ fetch('/getTransactionHist', {
 })
 .then(response => response.json())
 .then(data => {
-  for (const transaction of data.data.slice(0, 6)) {
+  transactions = data.data
+  for (const transaction of transactions.slice(0, 6)) {
     if (transaction.crban == bankID) {
       document.getElementById("transactionDisplay").innerHTML += 
       `<tr>
@@ -27,6 +34,21 @@ fetch('/getTransactionHist', {
       </tr>`
     }
   }
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+fetch('/getAccountBalance', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({bankID: bankID}),
+})
+.then(response => response.json())
+.then(data => {
+  document.getElementById("accountBalance").innerText += data.data.acct_balance
 })
 .catch((error) => {
   console.error('Error:', error);
