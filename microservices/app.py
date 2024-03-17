34c782ec80842a4e6540f40a74e360a570d7d3d1
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2024-03-17 11:16:37 +0800

    chatbot with rasa

--- microservices/app.py
+++ microservices/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 import json
 from invokes import invoke_http
+
+RASA_API_URL = 'http://localhost:5005/webhooks/rest/webhook'
 
 app = Flask(__name__, template_folder="../client/templates", static_folder="../client/static")
 
@@ -12,8 +14,23 @@ def home():
 @app.route("/getTransactionHist", methods=['POST'])    
 def getTransactionHist():
     bankID = request.json['bankID']
-    transactionHist = invoke_http("http://127.0.0.1:5003/transactionHistory/bank_acct_id/" + str(bankID), method='GET')
+    transactionHist = invoke_http("http://127.0.0.1:5002/transactionHistory/bank_acct_id/" + str(bankID), method='GET')
     return transactionHist
+
+@app.route("/roboadvisor")
+def roboadvisor():
+    return render_template("roboadvisor.html")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    user_message = request.json['message']
+
+    # Here, you could add logic to generate a response to the message
+    rasa_response = requests.post(RASA_API_URL, json={'message': user_message})
+    rasa_response_json = rasa_response.json()
+
+    bot_response = rasa_response_json[0]['text'] if rasa_response_json else 'Sorry, I didn\'t understand that.'
+    return jsonify({'response': bot_response })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
