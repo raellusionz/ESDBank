1c56a9283f69e3986d05f4b4b150f2555465b241
raellusionz <146325019+raellusionz@users.noreply.github.com>
2024-03-19 01:15:47 +0800

    Pray the create and update target works

--- microservices/user_goal.py
+++ microservices/user_goal.py
@@ -24,19 +24,38 @@ class user_preference_logs(db.Model):
     __tablename__ = 'user_prefers_table'
 
     bank_acct_id = db.Column(db.String(20), primary_key=True)
-    saving_goal_percent = db.Column(db.DECIMAL(15,2))
-    necessity_expenditure_percent = db.Column(db.DECIMAL(15,2))
+    saving_goal_percentage = db.Column(db.DECIMAL(15,2))
+    necessity_expenditure_percentage = db.Column(db.DECIMAL(15,2))
+    fund_transfer = db.Column(db.DECIMAL(15,2))
+    necessities = db.Column(db.DECIMAL(15,2))
+    transportation = db.Column(db.DECIMAL(15,2))
+    f_b = db.Column(db.DECIMAL(15,2))
+    shop_entertain = db.Column(db.DECIMAL(15,2))
+    others = db.Column(db.DECIMAL(15,2))
 
-    def __init__(self, bank_acct_id, saving_goal_percent, necessity_expenditure_percent):
+    def __init__(self, bank_acct_id, saving_goal_percentage, necessity_expenditure_percentage,fund_transfer,necessities, transportation, f_b, shop_entertain, others):
         self.bank_acct_id = bank_acct_id
-        self.saving_goal_percent = saving_goal_percent
-        self.necessity_expenditure_percent = necessity_expenditure_percent
+        self.saving_goal_percentage = saving_goal_percentage
+        self.necessity_expenditure_percentage = necessity_expenditure_percentage
+        self.fund_transfer = fund_transfer
+        self.necessities = necessities
+        self.transportation = transportation
+        self.f_b = f_b
+        self.shop_entertain = shop_entertain
+        self.others = others
+
 
     def json(self):
         return {
             "bank_acct_id": self.bank_acct_id, 
-            "saving_goal_percent": self.saving_goal_percent,
-            "necessity_expenditure_percent": self.necessity_expenditure_percent}
+            "saving_goal_percentage": self.saving_goal_percentage,
+            "necessity_expenditure_percentage": self.necessity_expenditure_percentage,
+            "fund_transfer": self.fund_transfer,
+            "necessities": self.necessities,
+            "transportation": self.transportation,
+            "f_b" : self.f_b,
+            "shop_entertain" : self.shop_entertain,
+            "others": self.others}
 
 @app.route("/")
 def homepage():
@@ -75,12 +94,12 @@ def get_saving_percent(id_num):
     	limit(1)
         ).first()
     if account:
-        saving_goal_percent = account.json()["saving_goal_percent"]
+        saving_goal_percentage = account.json()["saving_goal_percentage"]
         return jsonify(    
             {
                 "code": 200,
                 "data": {
-                    "saving_goal_percent": saving_goal_percent
+                    "saving_goal_percent": saving_goal_percentage
                 }
             }
         )
@@ -101,12 +120,27 @@ def get_expenditure_percent(id_num):
     	limit(1)
         ).first()
     if account:
-        necessity_expenditure_percentage = account.json()["necessity_expenditure_percent"]
+        necessity_expenditure_percentage = account.json()["necessity_expenditure_percentage"]
+        fund_transfer = account.json()["fund_transfer"]
+        necessities = account.json()["necessities"]
+        transportation = account.json()["transportation"]
+        f_b = account.json()["f_b"]
+        shop_entertain = account.json()["shop_entertain"]
+        others = account.json()["others"]
+       
         return jsonify(    
             {
                 "code": 200,
                 "data": {
-                    "expanditure_target_percent": necessity_expenditure_percentage
+                    "expanditure_target_percent": necessity_expenditure_percentage,
+                    "breakdown": {
+                        "fund_transfer": fund_transfer,
+                        "necessities": necessities,
+                        "transportation": transportation,
+                        "f_b": f_b,
+                        "shop_entertain": shop_entertain,
+                        "others": others
+                    }
                 }
             }
         )
@@ -119,7 +153,7 @@ def get_expenditure_percent(id_num):
 
 
 #Updating user saving goals percent
-@app.route("/userPreference/goal_perc_update/<string:id_num>/<float:saving_percent>",methods=['PUT'])
+@app.route("/userPreference/update_saving_goal/<string:id_num>/<float:saving_percent>",methods=['PUT'])
 def update_saving_goal(id_num,saving_percent):
     account = db.session.scalars(
     	db.select(user_preference_logs).filter_by(bank_acct_id=id_num).
@@ -127,18 +161,38 @@ def update_saving_goal(id_num,saving_percent):
         ).first()
     
     if account:
-        account.saving_goal_percent = saving_percent
-        db.session.commit()
-        return jsonify(
-            {
-                "code": 200,
-                "message": "Saving Goal Target Updated Successfully.",
-                "data": 
+        saving_goal_percentage = account["saving_goal_percentage"]
+        necessity_expenditure_percentage = account.json()["necessity_expenditure_percentage"]
+        total = saving_percent + necessity_expenditure_percentage
+        
+        if (saving_goal_percentage != 0) and (necessity_expenditure_percentage !=0):
+            if(total<=100):
+                account.saving_goal_percentage = saving_percent
+                db.session.commit()
+                return jsonify(
                     {
-                        "account": account.json()
+                        "code": 200,
+                        "message": "Saving Goal Updated Successfully.",
+                        "data": 
+                            {
+                                "account": account.json()
+                            }
                     }
-            }
-        )
+                )
+            else:
+                return jsonify(
+                    {
+                        "code": 403,
+                        "message": "Please re-assign your saving goals, value is greater than your expenditure goals. Ensure it totals up to 100%"
+                    }
+                )  
+        else:
+            return jsonify(
+                {
+                    "code": 403,
+                    "message": "You have not set a goal. Please create a new goal!"
+                }
+            )
     return jsonify(
         {
             "code": 404,
@@ -148,25 +202,51 @@ def update_saving_goal(id_num,saving_percent):
 
 
 #Updating necessity expenditure percent
-@app.route("/userPreference/expenditure_update/<string:id_num>/<float:expenditure_percent>",methods=['PUT'])
-def update_expenditure_target(id_num,expenditure_percent):
+@app.route("/userPreference/update_expenditure_target/<string:id_num>/<float:fund_transfer>/<float:necessities>/<float:transportation>/<float:f_b>/<float:shop_entertain>/<float:others>",methods=['PUT'])
+def update_expenditure_target(id_num,fund_transfer,necessities,transportation,f_b,shop_entertain,others):
+
+    total_expenditure_percent = fund_transfer + necessities + transportation + f_b + shop_entertain + others
+        
     account = db.session.scalars(
     	db.select(user_preference_logs).filter_by(bank_acct_id=id_num).
     	limit(1)
         ).first()
     if account:
-        account.necessity_expenditure_percent = expenditure_percent
-        db.session.commit()
-        return jsonify(
-            {
-                "code": 200,
-                "message": "Expenditure Target Updated Successfully.",
-                "data": 
+        saving_goal_percentage = account["saving_goal_percentage"]
+        necessity_expenditure_percentage = account["necessity_expenditure_percentage"]
+        if (saving_goal_percentage != 0) and (necessity_expenditure_percentage !=0):
+            if(total_expenditure_percent<=100):
+                account.fund_transfer = fund_transfer
+                account.necessities = necessities
+                account.transportation = transportation
+                account.f_b = f_b
+                account.shop_entertain = shop_entertain
+                account.others = others
+                db.session.commit()
+                return jsonify(
                     {
-                        "account": account.json()
+                        "code": 200,
+                        "message": "Expenditure Target Updated Successfully.",
+                        "data": 
+                            {
+                                "account": account.json()
+                            }
                     }
-            }
-        )
+                )
+            else:
+                return jsonify(
+                    {
+                        "code": 403,
+                        "message": "Please re-assign your expenditure goals, value is greater than your expenditure goals. Ensure it totals up to 100%"
+                    }
+                )
+        else:
+            return jsonify(
+                {
+                    "code": 403,
+                    "message": "You have not set a goal. Please create a new goal!"
+                }
+            )
     return jsonify(
         {
             "code": 404,
@@ -174,6 +254,120 @@ def update_expenditure_target(id_num,expenditure_percent):
         }
     ), 404
 
+
+#Updating a complete new user saving goals percent
+@app.route("/userPreference/update_new_user_preference_goal/<string:id_num>/<float:saving_percent>/<float:fund_transfer>/<float:necessities>/<float:transportation>/<float:f_b>/<float:shop_entertain>/<float:others>",methods=['PUT'])
+
+def update_new_user_preference_goal(id_num,saving_percent,fund_transfer,necessities,transportation,f_b,shop_entertain,others):
+    account = db.session.scalars(
+    	db.select(user_preference_logs).filter_by(bank_acct_id=id_num).
+    	limit(1)
+        ).first()
+    
+    if account:
+        saving_goal_percentage = account["saving_goal_percentage"]
+        necessity_expenditure_percentage = account.json()["necessity_expenditure_percentage"]
+        
+        if (saving_goal_percentage != 0) and (necessity_expenditure_percentage !=0):
+            total_expenditure_percent = fund_transfer + necessities + transportation + f_b + shop_entertain + others
+            total = saving_percent + total_expenditure_percent
+            if (total <= 100): #less or equal to 100
+                account.saving_goal_percentage = saving_percent
+                account.fund_transfer = fund_transfer
+                account.necessities = necessities
+                account.transportation = transportation
+                account.f_b = f_b
+                account.shop_entertain = shop_entertain
+                account.others = others
+                db.session.commit()
+                return jsonify(
+                    {
+                        "code": 200,
+                        "message": "New Saving Goal Target Updated Successfully.",
+                        "data": 
+                            {
+                                "account": account.json()
+                            }
+                    }
+                )
+            else:
+                return jsonify(
+                    {
+                        "code": 403,
+                        "message": "Please re-assign your saving and expenditure goals. Ensure it totals up to 100%"
+                    }
+                )
+        else:
+            return jsonify(
+                {
+                    "code": 403,
+                    "message": "You have not set a goal. Please create a new goal!"
+                }
+            )
+
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Bank account not found."
+        }
+    ), 404
+
+
+#Create a complete new user saving goals percent
+@app.route("/userPreference/create_new_user_preference_goal/<string:id_num>/<float:saving_percent>/<float:fund_transfer>/<float:necessities>/<float:transportation>/<float:f_b>/<float:shop_entertain>/<float:others>",methods=['PUT'])
+
+def create_new_user_preference_goal(id_num,saving_percent,fund_transfer,necessities,transportation,f_b,shop_entertain,others):
+    account = db.session.scalars(
+    	db.select(user_preference_logs).filter_by(bank_acct_id=id_num).
+    	limit(1)
+        ).first()
+    if account:
+        saving_goal_percentage = account["saving_goal_percentage"]
+        necessity_expenditure_percentage = account.json()["necessity_expenditure_percentage"]
+        
+        if (saving_goal_percentage == 0) and (necessity_expenditure_percentage ==0):
+            total_expenditure_percent = fund_transfer + necessities + transportation + f_b + shop_entertain + others
+            total = saving_percent + total_expenditure_percent
+            if (total <= 100): #less or equal to 100
+                account.saving_goal_percentage = saving_percent
+                account.fund_transfer = fund_transfer
+                account.necessities = necessities
+                account.transportation = transportation
+                account.f_b = f_b
+                account.shop_entertain = shop_entertain
+                account.others = others
+                db.session.commit()
+                return jsonify(
+                    {
+                        "code": 201,
+                        "message": "New Saving Goal Target Created Successfully.",
+                        "data": 
+                            {
+                                "account": account.json()
+                            }
+                    }
+                )
+            else:
+                return jsonify(
+                    {
+                        "code": 403,
+                        "message": "Please re-assign your saving and expenditure goals. Ensure it totals up to 100%"
+                    }
+                )
+        else:
+            return jsonify(
+                {
+                    "code": 403,
+                    "message": "You already have set your goals. Update your goals instead!"
+                }
+            )
+
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Bank account not found."
+        }
+    ), 404
 
 if __name__ == '__main__':
     # app.run(port=5000, debug=True)
